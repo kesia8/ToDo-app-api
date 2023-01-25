c7620bca81d807955317f25815d631dc02cd8cc6
Kesia <112557978+kesia8@users.noreply.github.com>
2023-01-25 11:10:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Rota ativada com GET e valores de usuarios devem ser retornados');
+})
+
+app.get('/', (req, res) => {
+    res.send('Rota ativada com GET e valores de tarefas devem ser retornados');
+})
+
+app.listen(port, () => {
+    console.log(`porta ${port}`);
+})
+
+
+
+
+
+
+
